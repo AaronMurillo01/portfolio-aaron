352186e5478ef399b1a69049648952c9fc72664b
Aaron Murillo <amurillo11@cnm.edu>
2023-01-18 11:30:55 -0700

    adding project wiht react build in sidebar and logo

---
+++ portfolio-aaron/src/components/Sidebar/index.js
@@ -0,0 +1,27 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import AaronLogo from '../src/assets/images/logo_a.png';
+import LogoSubtitle from '../src/assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={AaronLogo} alt="logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="aaron" />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+               <FontAwesomeIcon icon={faHome} color="#4d4d4e" /> 
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+               <FontAwesomeIcon icon={faUser} color="#4d4d4e" /> 
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+               <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" /> 
+            </NavLink>
+        </nav>
+    </div>
+)
+export default Sidebar;
